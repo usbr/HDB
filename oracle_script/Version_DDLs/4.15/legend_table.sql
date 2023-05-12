67170e08b509445924a67ad393ae4481785c7327
xcellious <38482887+xcellious@users.noreply.github.com>
2023-05-12 09:47:19 -0400

    Changed table name to REF_LEGEND

--- oracle_script/Version_DDLs/4.15/legend_table.sql
+++ oracle_script/Version_DDLs/4.15/legend_table.sql
@@ -24,43 +24,43 @@
 --#### END of hdb poet source code ####
 --------------------------------------------------------
 
-CREATE TABLE legend (
+CREATE TABLE REF_LEGEND (
   color VARCHAR2(20) NOT NULL,
   description VARCHAR2(200) NOT NULL,
   CONSTRAINT pk_legend PRIMARY KEY (color)
 ) tablespace HDB_DATA ;
     
-  GRANT SELECT,INSERT,UPDATE,DELETE ON LEGEND TO CALC_DEFINITION_ROLE;
-  GRANT SELECT,INSERT,UPDATE,DELETE ON LEGEND TO SAVOIR_FAIRE;
-  GRANT SELECT,INSERT,UPDATE,DELETE ON LEGEND TO HDB_META_ROLE;
-  GRANT REFERENCES ON LEGEND TO DECODES;
-  GRANT SELECT ON LEGEND TO PUBLIC;
-  create public synonym LEGEND for LEGEND;
+  GRANT SELECT,INSERT,UPDATE,DELETE ON REF_LEGEND TO CALC_DEFINITION_ROLE;
+  GRANT SELECT,INSERT,UPDATE,DELETE ON REF_LEGEND TO SAVOIR_FAIRE;
+  GRANT SELECT,INSERT,UPDATE,DELETE ON REF_LEGEND TO REF_META_ROLE;
+  GRANT REFERENCES ON REF_LEGEND TO DECODES;
+  GRANT SELECT ON REF_LEGEND TO PUBLIC;
+  create public synonym REF_LEGEND for REF_LEGEND;
   
   
 --For UC only. Other databases will expect to fail.  
-  GRANT SELECT ON LEGEND TO HDB_READ_ROLE;
-  GRANT SELECT,INSERT,UPDATE,DELETE ON LEGEND TO HDB_WRITE_ROLE;
+  GRANT SELECT ON REF_LEGEND TO HDB_READ_ROLE;
+  GRANT SELECT,INSERT,UPDATE,DELETE ON REF_LEGEND TO HDB_WRITE_ROLE;
 
 --populate colors and descriptions  
- INSERT INTO legend (color, description) VALUES ('PaleGreen', 'source data/not computed or manually edited/no overwrite flag set');
- INSERT INTO legend (color, description) VALUES ('DarkSeaGreen', 'source data/not computed or manually edited/overwrite flag set');
- INSERT INTO legend (color, description) VALUES ('Yellow', 'computed data from computation processor/not an aggregation from one interval to another');
- INSERT INTO legend (color, description) VALUES ('Gold', 'computed data from computation processor/is an aggregation from one interval to another');
- INSERT INTO legend (color, description) VALUES ('Goldenrod', 'computed data from computation processor/overwrite flag set');
- INSERT INTO legend (color, description) VALUES ('PaleGoldenrod', 'RiverWare computed data/no overwrite flag set');
- INSERT INTO legend (color, description) VALUES ('DarkKhaki', 'RiverWare computed data/overwrite flag set');
- INSERT INTO legend (color, description) VALUES ('SkyBlue', 'manually edited data/no overwrite flag set');
- INSERT INTO legend (color, description) VALUES ('RoyalBlue', 'manually edited data/overwrite flag set');
- INSERT INTO legend (color, description) VALUES ('Aquamarine', 'RiverWare manually edited data/no overwrite flag set');
- INSERT INTO legend (color, description) VALUES ('DarkTurquoise', 'RiverWare manually edited data/overwrite flag set');
- INSERT INTO legend (color, description) VALUES ('LightGray', 'missing data/not yet inserted');
- INSERT INTO legend (color, description) VALUES ('Red', 'Validation Failed!!! - Data outside of acceptable parameters');
- INSERT INTO legend (color, description) VALUES ('DarkOrange', 'RBase data not available - computations and undo unavailable');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('PaleGreen', 'source data/not computed or manually edited/no overwrite flag set');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('DarkSeaGreen', 'source data/not computed or manually edited/overwrite flag set');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('Yellow', 'computed data from computation processor/not an aggregation from one interval to another');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('Gold', 'computed data from computation processor/is an aggregation from one interval to another');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('Goldenrod', 'computed data from computation processor/overwrite flag set');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('PaleGoldenrod', 'RiverWare computed data/no overwrite flag set');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('DarkKhaki', 'RiverWare computed data/overwrite flag set');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('SkyBlue', 'manually edited data/no overwrite flag set');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('RoyalBlue', 'manually edited data/overwrite flag set');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('Aquamarine', 'RiverWare manually edited data/no overwrite flag set');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('DarkTurquoise', 'RiverWare manually edited data/overwrite flag set');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('LightGray', 'missing data/not yet inserted');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('Red', 'Validation Failed!!! - Data outside of acceptable parameters');
+ INSERT INTO REF_LEGEND (color, description) VALUES ('DarkOrange', 'RBase data not available - computations and undo unavailable');
 commit;
 
 --update version table
-INSERT INTO TSDB_PROPERTY (PROP_NAME, PROP_VALUE) VALUES ('4.15.5', 'Legend table have been created.');
+INSERT INTO TSDB_PROPERTY (PROP_NAME, PROP_VALUE) VALUES ('4.15.5', 'REF_LEGEND table have been created.');
 
 --Commit
 commit;
